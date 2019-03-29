a075fc8bb3a4bcc4ab2c73c1ed2ef698a05f1d43
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2019-03-29 03:06:31 -0700

    route working with example request to lob

--- routes/createPostcard.js
+++ routes/createPostcard.js
@@ -3,18 +3,18 @@ var router = express.Router();
 var Lob = require('lob')('test_066ca30f3d0fe263d7b6a8af760a4983778');
 /* Post a card */
 router.post('/createPostcard', function(req, res, next) {
-console.log('i am in the users!')
+console.log('in the create postcard', req.body.pdf.filePath)
     Lob.postcards.create({
     description: 'Demo Postcard job',
     to: {
-    name: 'Harry Zhang',
+    name: 'Idalia',
     address_line1: '185 Berry St',
     address_line2: '# 6100',
     address_city: 'San Francisco',
     address_state: 'CA',
     address_zip: '94107'
   },
-    // front: fs.createReadStream('/path/to/your/front.pdf'),
+    // front: fs.createReadStream(req.body.pdf.filePath),
     // back: fs.createReadStream('/path/to/your/back.pdf')
     front: 'https://lob.com/postcardfront.pdf',
     back: 'https://lob.com/postcardback.pdf'
